585af3318b8eef71049026115f99ffd7b206ab03
ChangHyun <karlll@naver.com>
2024-02-27 12:36:57 +0900

    점심 시간

--- day05/p34_addrBook.py
+++ day05/p34_addrBook.py
@@ -1,38 +1,54 @@
 # file: p34_addrBook.py
 # desc: 콘솔 주소록 프로그램
-import os
+import os 
 
-
-class Contact: #주소록 클래스
+class Contact: # 주소록 클래스
     # 생성자
-    def __init__(self,name, phoneNumber, eMail, addr) -> None:
+    def __init__(self, name, phoneNumber, eMail, addr) -> None:
         self.__name = name
         self.__phoneNumber = phoneNumber
         self.__eMail = eMail
         self.__addr = addr
 
-# 사용자가 원하는 형태로 출력
-    def __str__(self) -> str: #
-        res = (f'이  름 : {self.__name}\n'    # 원래 출력 <__main__.Contact object at 0x0000016B505E2150>
+    # 사용자가 원하는 형태로 출력
+    def __str__(self) -> str: # 원래출력 <__main__.Contact object at 0x0000024500772150> 
+        res = (f'이  름 : {self.__name}\n'
                f'핸드폰 : {self.__phoneNumber}\n'
                f'이메일 : {self.__eMail}\n'
                f'주  소 : {self.__addr}')
         return res
-    
+
+    # 연락처 여부확인
+    def isNameExist(self,name):
+        if self.__name==name: #찾는 이름 존재
+            return True
+        else:
+            return False
+        
+    def getInfo(self):
+        return self.__name,self.__phoneNumber,self.__eMail,self.__addr
+
 def setContact(): # 사용자 입력으로 주소록 받기함수
-    (name, phoneNumber, eMail, addr) = input('주소록 입력(이름, 핸드폰, 이메일, 주소[구분자/] >)').split('/')
-    name=name.strip()
-    phoneNumber=phoneNumber.strip()
-    eMail=eMail.strip()
-    addr=addr.strip()
-    # print(f'"{name}","{phoneNumber}","{eMail}","{addr}"')
-    contact = Contact(name, phoneNumber, eMail, addr) #매개변수명과 동일하게 로컬변수 이름 지정
+    (name, phoneNumber, eMail, addr) = input('입력(이름,핸드폰,이메일,주소[/])> ').split('/')
+    name = name.strip() # 사용자실수로 들어간 스페이스 제거
+    phoneNumber = phoneNumber.strip()
+    eMail = eMail.strip()
+    addr = addr.strip()
+    # print(f'"{name}", "{phoneNumber}", "{eMail}", "{addr}"')
+    contact = Contact(name, phoneNumber, eMail, addr) # 매개변수명과 동일하게 로컬변수 이름 지정
     return contact
 
-def clearConsole():
-    cmd = 'clear' # macOS, Linux, Unix 명령어
-    if os.name in('nt', 'dos'): # window
-        cmd = 'cls' #window 명령어
+def delContact(lst, name): #연락처 삭제함수
+    for i, item in enumerate(lst):
+        if item.isNameExist(name):
+            del lst[i]
+
+def saveContact(lst): #연락처 저장함수
+    with open(file= './contacts.txt', mode='w',encoding='utf-8') as fp:
+        for item in lst:
+            name,phoneNumber,eMail,addr = item.getInfo()
+            fp.write(f'{name}/{phoneNumber}/{eMail}/{addr}\n')
+            
 
 def displayMenu():
     menu = ('주소록 프로그램\n'
@@ -44,34 +60,49 @@ def displayMenu():
     sel = int(input('메뉴입력 : '))
     return sel
 
-def getContacts(lst):
-    for item in lst:
+def clearConsole():
+    cmd = 'clear' # macOS, Linux, Unix 명령어
+    if os.name in ('nt', 'dos'): # window
+        cmd = 'cls' # window 명령어
+
+    os.system(cmd) # 명령어 실행
+
+def getContacts(lst): # 리스트를 받아서 출력함수
+    for i, item in enumerate(lst):
+        print(f'{i+1}==========>')
         print(item)
 
 def run():
-    #연락처담을 주소록 리스트
+    # 연락처담을 주소록 리스트
     lstContact = []
 
     clearConsole() # 화면을 클리어
     while True:
         selMenu = displayMenu()
-        if selMenu ==1: #연락처 추가라면
+
+        if selMenu == 1: # 연락처 추가라면
             clearConsole()
             contact = setContact()
             lstContact.append(contact)
-            #print(lstContact)
+            # print(lstContact)
+            input(); clearConsole() # 엔터 입력유도            
+        elif selMenu == 2: # 연락처 출력
+            clearConsole()
+            getContacts(lstContact)
+            input(); clearConsole()
+        elif selMenu == 3: # 연락처 삭제
+            clearConsole()
+            name=input('삭제할 이름 입력 : ')
+            delContact(lstContact, name)
             input(); clearConsole()
         elif selMenu == 4:
+            saveContact(lstContact)
             break
         else:
-            clearConsole()
+            clearConsole() 
 
-# def run():
-#     first = Contact(name='홍길동', phoneNumber='010-9999-5555', eMail='hgd@naver.com', addr='경성')
-#     print(first)
-
-if __name__=='__main__': #메인 엔트리
+if __name__ == '__main__': # 메인엔트리
     print('프로그램 시작')
-    run() #메인함수 실행
+    run() # 메인함수 실행
 
 print('프로그램 종료')
