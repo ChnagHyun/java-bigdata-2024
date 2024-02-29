0f85c779557f59560a34d70b02f77ba7a3ab484a
ChangHyun <karlll@naver.com>
2024-02-29 16:34:49 +0900

    3일 휴무

---
+++ day07/p48_transApp.py
@@ -0,0 +1,44 @@
+# file : p48_transApp.py
+# desc : PyQt5 스레드 학습용(스레드 사용안함)
+#pip install googletrans
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from googletrans import Translator
+
+class qtApp(QWidget):
+    def __init__(self) -> None: 
+        super().__init__()
+        self.initUI()
+        self.myTrans = Translator() # 구글번역기 객체 생성
+
+    def initUI(self): # ui파일을 로드해서 화면디자인 사용
+        uic.loadUi('./day07/transApp.ui', self)
+        self.setWindowTitle('구글번역앱')
+        self.setWindowIcon(QIcon('./images/windows.png'))
+        # 버튼 시그널처리 
+        self.btnTrans.clicked.connect(self.btnTransClicked) # ui파일내 위젯은 자동완성X 
+        self.txtBaseText.returnPressed.connect(self.btnTransClicked)
+        self.show()
+
+    def btnTransClicked(self):
+        # QMessageBox.about(self, '번역','번역시작!!')
+        text = self.txtBaseText.text().strip()
+        if len(text) != 0:
+            result = self.myTrans.translate(text, src='auto',dest='en')
+            self.txbResult.append(result.text)
+
+    def closeEvent(self, QCloseEvent) -> None:
+        re = QMessageBox.question(self, '종료확인', '종료하시겠습니까?', QMessageBox.Yes|QMessageBox.No)
+        if re == QMessageBox.Yes: 
+            QCloseEvent.accept() # 진짜 닫음
+        else:
+            QCloseEvent.ignore() # 무시        
+
+app = QApplication(sys.argv) 
+inst = qtApp()
+app.exec_()
